1265ea881ecd4d4c23b831f7b87a1064a4d18d7f
sollendy <edlannino@tutanota.com>
2023-03-01 18:12:10 +0100

    esercizio ultimato

--- js/script.js
+++ js/script.js
@@ -3,13 +3,17 @@
 // improvviso perché non so più come ordinare le operazioni
 
 // creo una lista della spesa in un array
-const listaSpesa = [' banane', ' arance', ' mele', ' uova', ' latte di soia', ' riso', ' lattuga', ' sparacello', ' finocchio', ' carote'];
+const listaSpesa = [' banane;', ' arance;', ' mele;', ' uova;', ' latte di soia;', ' riso;', ' lattuga;', ' sparacello;', ' finocchio;', ' carote.'];
+let listaEl = document.getElementById("lista");
 let i = 0;
 
 //console.log(listaSpesa);
 
 while (i < listaSpesa.length) {
+    let newLiElement = document.createElement('li');
+    listaEl.append(newLiElement);
+    newLiElement.innerHTML = (listaSpesa[i] + "<br>");
     //console.log(listaSpesa[i]);
-    document.writeln(listaSpesa[i]);
+    //document.writeln(listaSpesa[i]);
     i++;
 }
