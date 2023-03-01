501b26bf40b2fb58f65c253d4b5099edef5a8034
sollendy <edlannino@tutanota.com>
2023-03-01 17:01:05 +0100

    .

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 // creo una lista della spesa in un array
 // creo una variabile contatore equivalente al valore 0 del array
-// improvviso perché non so più come ordinare le operazioni 
+// improvviso perché non so più come ordinare le operazioni
+
+// creo una lista della spesa in un array
+const listaSpesa = [' banane', ' arance', ' mele', ' uova', ' latte di soia', ' riso', ' lattuga', ' sparacello', ' finocchio', ' carote'];
+let = contatore = 0;
+console.log(listaSpesa);
